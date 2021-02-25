b3dc958bc7851dc571dff6e39d5897557d9670ac
myQwil <myQwil@gmail.com>
2021-02-25 02:44:41 -0500

    removed "building-libpd" folder
    opted instead for a .diff file to be applied to user's libpd folder

--- building-libpd/extra/ekext/ekext.c
+++
@@ -1,8 +0,0 @@
-void cup_setup(void);
-void cupd_setup(void);
-
-void ekext_setup(void)
-{
-	cup_setup();
-	cupd_setup();
-}
--- building-libpd/extra/myqwil/ntof.h
+++
@@ -1,6 +0,0 @@
-#include "m_pd.h"
-#include <math.h>
-
-static t_float ntof(t_float f, double root, double semi) {
-	return (root * exp(semi*f));
-}
